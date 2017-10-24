d1dcf540576a365e720010fdf437acee46708c0c
= <amar.kazaz@gmail.com>
2017-10-24 17:09:31 +0200

    API for location with small errors

--- forms.py
+++ forms.py
@@ -9,3 +9,13 @@ class SignupForm(Form):
 	email = StringField('Email', validators=[DataRequired('Please enter your email'), Email('Invalid Email')])
 	password = PasswordField('Password', validators=[DataRequired('Please enter your password'), Length(min=6, message="Password must be 6 characters") ])
 	submit = SubmitField('Sign up')	
+
+class LoginForm(Form):
+	"""docstring for LoginForm"""
+	email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter mail in valid format")])
+	password = PasswordField('Password', validators=[DataRequired("Please enter a password.")])
+	submit = SubmitField("Sign In")
+
+class AddressForm(Form):
+	address = StringField('Address', validators=[DataRequired("Please enter an address")])
+	submit = SubmitField("Search")
--- models.py
+++ models.py
@@ -1,6 +1,10 @@
 from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug import generate_password_hash, check_password_hash
 
+import geocoder
+import urllib.request
+import simplejson as json
+
 db = SQLAlchemy()
 
 class User(db.Model):
@@ -23,3 +27,49 @@ class User(db.Model):
 
 	def check_password(self, password):
 		return check_password_hash(self.pwdhash, password)
+
+class Place(object):
+	"""docstring for Place"""
+	def meters_to_walking_time(self, meters):
+		return int(meters/80)
+
+	def wiki_path(self, slug):
+		return urllib.request.urlparse.urljoin("http://en.wikipedia.org/wiki/", slug.replace(' ', '_'))
+	
+	def address_to_latlng(self, address):
+		g = geocoder.google(address)
+		return (g.lat, g.lng)
+
+	def query(self, address):
+		lat, lng = self.address_to_latlng(address)
+		print(lat, lng)
+
+		query_url = 'https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=10000&gscoord=37.786971%7C-122.399677&format=json'
+		g = urllib.request.urlopen(query_url)
+		results = g.read()
+		g.close()
+
+		data = json.loads(results)
+		print (data)
+
+		places = []
+
+		for place in data['query']['geosearch']:
+			name = place['title']
+			meters = place['dist']
+			lat = place['lat']
+			lng = place['lon']
+
+			wiki_url = self.wiki_path(name)
+			walking_time = self.meters_to_walking_time(meters)
+			d = {
+				'name': name,
+				'url': wiki_url,
+				'time': walking_time,
+				'lat': lat,
+				'lng': lng
+			}
+
+			places.append(d)
+
+		return places
